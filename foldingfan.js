01106660e7144204edc6e1de01030785c4ac03db
Marina Kukso <marina.kukso@gmail.com>
2016-11-27 17:18:25 -1000

    +working demos bye, feedbackeffect, foldingfan. bye and foldingfan need different movement

---
+++ foldingfan.js
@@ -0,0 +1,14 @@
+const regl = require('regl')()
+const mat4 = require('gl-mat4')
+const mod = require('./libraries/foldingfanmodule.js')(regl)
+const camera = require('./libraries/camera.js')(regl, {
+  center: [0.0, 2.5, 0.0]
+})
+const SetupCamera = regl  
+
+regl.frame(() => {
+  regl.clear({
+    color: [0, 0, 0, 1]
+  })
+  camera(() => { mod() })
+})
