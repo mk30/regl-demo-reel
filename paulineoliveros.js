2c75d4b99a9b585d049fedc10617dd636a8e4888
Marina Kukso <marina.kukso@gmail.com>
2016-12-01 18:13:35 -1000

    paulineoliveros: added warp and color changes

--- paulineoliveros.js
+++ paulineoliveros.js
@@ -9,7 +9,7 @@ const textMesh = vectorizeText('thank you pauline oliveros', {
   textBaseline: 'middle'
 })
 const camera = require('./libraries/camera.js')(regl, {
-  center: [0, 3, 0],
+  center: [0, 0, 0],
   distance: 6 
 })
 function text (regl){
@@ -18,17 +18,23 @@ function text (regl){
   return regl({
     frag: `
       precision mediump float;
+      uniform float t;
       void main () {
-        gl_FragColor = vec4(0.2, 0.3, 0.7, 1.0);
+        gl_FragColor = vec4(sin(t), 0.3, 0.7, 1.0);
       }`,
     vert: `
       precision mediump float;
       uniform mat4 model, projection, view;
       attribute vec2 position;
       uniform float t;
+      vec2 warp (vec2 p){
+        float r = length(p.xy)*sin(t);
+        float theta = atan(p.y, p.x);
+        return vec2 (r*cos(theta), -r*sin(theta));
+      }
       void main () {
         gl_Position = projection * view * model *
-        vec4(position.x, -position.y, 0, 1.0);
+        vec4(warp(position), 0, 1.0);
       }`,
     attributes: {
       position: mesh.positions
@@ -40,7 +46,8 @@ function text (regl){
          },
       model: function(context, props){
         var theta = context.tick/60
-        return mat4.rotateY(rmat, mat4.identity(rmat), 1.2)
+        mat4.rotateY(rmat, mat4.identity(rmat), theta)
+        return rmat
       }
     },
     primitive: "triangles"
