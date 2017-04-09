ebdb5f612ae321dbda3aee72596311537f3278f6
Marina Kukso <marina.kukso@gmail.com>
2017-04-08 20:42:27 -1000

    -cruft from foldingfan

--- foldingfan.js
+++ foldingfan.js
@@ -4,7 +4,6 @@ const mod = require('./libraries/foldingfanmodule.js')(regl)
 const camera = require('./libraries/camera.js')(regl, {
   center: [0.0, 2.5, 0.0]
 })
-const SetupCamera = regl  
 
 regl.frame(() => {
   regl.clear({
