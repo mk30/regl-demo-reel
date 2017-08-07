c24e4bd9cab093b8ed02f6bd609e0d69161e46d1
Marina Kukso <marina.kukso@gmail.com>
2017-08-06 15:59:37 -1000

    +deepergeometry with kate, cathello2.js, hatetoleave.js, iphone.js, catmugball.js

--- hatetoleave.js
+++ hatetoleave.js
@@ -11,7 +11,7 @@ var drawfeedback = feedback(regl, `
   }
 `)
 const feedBackTexture = regl.texture({})
-const textMesh = vectorizeText('nightmare cats', {
+const textMesh = vectorizeText('i hate to leave you', {
   triangles: true,
   width: 4,
   textAlign: 'center',
