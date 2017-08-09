9f2bdd6e67894fceda08f3078e93db579c6d5faf
Marina Kukso <marina.kukso@gmail.com>
2017-08-08 20:32:37 -1000

    emoji.js does a thing

---
+++ emoji.js
@@ -0,0 +1,60 @@
+const regl = require('regl')()
+const mat4 = require('gl-mat4')
+var fs = require('fs');
+var image = fs.readFileSync(__dirname + '/emoji.jpg', 'base64');
+var imageurl = 'data:image/png;base64,' + image
+const drawCube = regl({
+  frag: `
+  precision mediump float;
+  uniform sampler2D tex;
+  varying vec2 uv;
+  uniform float time;
+  void main () {
+    gl_FragColor = texture2D(tex,vec2(uv.x +
+    sin(time*2.0)/4.0, uv.y+cos(time*2.0)/(uv.y/1.3)));
+  }`,
+  vert: `
+  precision mediump float;
+  attribute vec2 position;
+  varying vec2 uv;
+  uniform float time;
+  void main() {
+    uv = position;
+    gl_Position = vec4(1.0 - 2.0 * position, 0, 1);
+  }`,
+  attributes: {
+    position: [
+      -2, 0,
+      0, -2,
+      2, 2
+    ]
+  },
+  uniforms: {
+    tex: regl.prop('texture'),
+    time: regl.context('time')
+  },
+  count: 3
+})
+
+require('resl')({
+  manifest: {
+    texture: {
+      type: 'image',
+      src: imageurl,
+      parser: (data) => regl.texture({
+        data: data,
+        mag: 'linear',
+        min: 'linear'
+      })
+    }
+  },
+  onDone: ({texture}) => {
+    regl.frame(() => {
+      regl.clear({
+        color: [0, 0, 0, 255],
+        depth: 1
+      })
+      drawCube({texture})
+    })
+  }
+})
